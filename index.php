28ac99f5bc9e33a137b853f91c3aa02ba4a91ed6
julka_lolz <julia-makara@wp.pl>
2020-09-06 15:09:24 +0200

    forms
    

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<form action="" method="post">
+<div>
+	<label for="Gebruikersnaam"><b>Gebruikersnaam</b><br></label>
+    <input type="text" placeholder="Vul in je gebruikersnaam" name="gebruikersnaam" required><br><br>
+
+    <label for="Wachtwoord"><b>Wachtwoord</b><br></label>
+    <input type="password" placeholder="Vul in je wachtwoord" name="wachtwoord" required><br><br>
+	        
+    <button type="submit">Login</button><br><br>
+
+	<a href="signup.php"> Geen accound? Click hier en maak een nieuwe aan.</a><br>
+
+	<a href="lostpsw.php"> Wachtwoord of gebruikersnaam vergeten? Vraag hier om een nieuwe.</a>
+</div> 
---
+++ lostpsw.php
@@ -0,0 +1,8 @@
+
+<form action="" method="post">
+<div>
+	<label for="E-mail"><b>E-mail</b><br></label>
+    <input type="text" placeholder="Vul in je e-mail" name="e-mail" required><br><br>
+
+    <button type="submit">Login</button>
+</div> 
