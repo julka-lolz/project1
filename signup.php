fe94c099cb882c9aa965a7e44193a1525e803ec2
julka_lolz <julia-makara@wp.pl>
2020-10-02 19:45:21 +0200

    updated

--- signup.php
+++ signup.php
@@ -1,21 +1,43 @@
-<?php
+﻿<?php
 
 include 'database.php';
 
 $db = new database('localhost', 'root', '', 'project1', 'utf8');
-$fieldnames = array("voornaam", "achternaam", "e-mail", "gebruikersnaam", "wachtwoord", "herhaal wachtwoord"); 
+$fieldnames = array(
+	$_POST["voornaam"], 
+	$_POST["achternaam"], 
+	$_POST["email"], 
+	$_POST["username"], 
+	$_POST["password"]	
+); 
 $error = false;
 
 foreach($fieldnames as $fieldname){
 	if (isset($_POST[$fieldname])){
 		$error = True;
+	} else {
+		if (!isset($_POST['submit'])) {
+			$voornaam = $_POST["voornaam"];
+			$tussenvoegsel = $_POST["tussenvoegsel"];
+			$achternaam = $_POST["achternaam"];
+			$email = $_POST["email"];
+			$username = $_POST["username"];
+			$password = $_POST["password"];
+			$query=mysqli_query(
+				"INSERT INTO 'account' AND 'persoon' (`voornaam`,`tussenvoegsel`,`achternaam`,`email`,`username`, 'password') 
+				VALUES ('$voornaam','$tussenvoegsel','$achternaam','$email','$username', '$password')"
+			);
+			echo ('<br>Treść została dodana<br>');
+		}
+						
 	}
 }
+
 
 ?>
 <html>	
 	<div>
-		<form action="signup.php" method="POST">
+		<form action="signup.php" method="$_POST">
 			<label for="Voornaam"><b>Voornaam</b><br></label>
 			<input type="text" placeholder="Vul in je voornaam" name="voornaam" required><br><br>
 			<label for="Tussenvoegsel"><b>Tussenvoegsel</b><br></label>
@@ -23,13 +45,13 @@ foreach($fieldnames as $fieldname){
 			<label for="Achternaam"><b>Achternaam</b><br></label>
 			<input type="text" placeholder="Vul in je achternaam" name="achternaam" required><br><br>
 			<label for="E-mail"><b>E-mail</b><br></label>
-			<input type="email" placeholder="Vul in je e-mail" name="e-mail" required><br><br>
+			<input type="email" placeholder="Vul in je e-mail" name="email" required><br><br>
 			<label for="Gebruikersnaam"><b>Gebruikersnaam</b><br></label>
-			<input type="text" placeholder="Vul in je gebruikersnaam" name="gebruikersnaam" required><br><br>
+			<input type="text" placeholder="Vul in je gebruikersnaam" name="username" required><br><br>
 			<label for="Wachtwoord"><b>Wachtwoord</b><br></label>
-			<input type="password" placeholder="Vul in je wachtwoord" name="wachtwoord" required><br><br>
+			<input type="password" placeholder="Vul in je wachtwoord" name="password" required><br><br>
 			<label for="Herhaal wachtwoord"><b>Herhaal wachtwoord</b><br></label>
-			<input type="password" placeholder="Herhaal je wachtwoord" name="herhaaal wachtwoord" required><br><br>   
+			<input type="password" placeholder="Herhaal je wachtwoord" name="password" required><br><br>   
 			<input type="submit">
 	</div>
 </html>
--- sql_statements.sql
+++ sql_statements.sql
@@ -4,25 +4,40 @@ create database project1;
 use project1;
 --hieronder maken we een tabel account aan.
 create table account(
-	id int not null,
+	id int not null AUTO_INCREMENT,
+	username varchar(255) not null UNIQUE,
 	email varchar(255) not null UNIQUE,
+	type int not null,
 	password varchar(255) not null,
-	primary key(id) 
+	created_at date not null,
+	updated_at date not null,
+	foreign key (type) references usertype(id),
+	primary key(id)	
+);
+
+-- hieronder maken we een tabel usertype aan.
+create table usertype(
+	id int not null AUTO_INCREMENT,
+	type varchar(255) not null,
+	created_at date not null,
+	updated_at date not null,
+	primary key(id)
 );
 --hieronder maken we een tabel persoon aan.
 create table persoon(
-	id int not null,
+	id int not null AUTO_INCREMENT,
 	account_id int,
 	voornaam varchar(255) not null,
-	tussenvoegsel varchar (255),
-	achternaam varchar (255) not null,
-	username varchar (255) not null,
-	primary KEY(id),
+	tussenvoegsel varchar(255),
+	achternaam varchar(255) not null,
+	created_at date not null,
+	updated_at date not null,
+	primary key(id),
 	foreign key (account_id) references account(id)
 );
 --hieronder maken we een admin als gebruiker door zijn gegevens toe te voegen aan de kollomen.
-insert into account(id,email,password)
-values(1,'admin1@google.com','e00cf25ad42683b3df678c61f42c6bda');
-insert into persoon(id,account_id,voornaam,tussenvoegsel,achternaam,username)
-values(1,1,'Julie','','Mak','admin1');
+insert into account(id, username, email, type, password, created_at, updated_at)
+values(1,'admin1','admin1@google.com',1,'e00cf25ad42683b3df678c61f42c6bda', '2020-10-02', '2020-10-02');
+insert into persoon(id,account_id,voornaam,tussenvoegsel,achternaam,created_at, updated_at)
+values(1,1,'Julie','','Mak','2020-10-02', '2020-10-02');
 
